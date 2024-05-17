bc9a6f693e584d7dbb9b446631d3668785964493
TrottieMcQueen <mcqueen.trottie007@gmail.com>
2024-05-17 08:57:13 -0500

    added function getting started to build

--- assistance-app/src/App.js
+++ assistance-app/src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
 function App() {
   return (
     <div className="App">
